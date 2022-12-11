d8ea06a1de13bc24d18c159aa4a323cf793a6274
BilalMir135 <bilalmir135@gmail.com>
2022-12-11 18:19:51 +0500

    chore(projects): commented adapters for test

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,5 +1,5 @@
 import UnipilotAdapter from "./unipilot";
-import UniswapAdapter from "./uniswap";
+// import UniswapAdapter from "./uniswap";
 import { types } from "@spockanalytics/base";
 
-export const Adapters: types.Adapter[] = [UnipilotAdapter, UniswapAdapter];
+export const Adapters: types.Adapter[] = [UnipilotAdapter];
