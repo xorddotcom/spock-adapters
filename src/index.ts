91aea9999e3853e81b51bd116677b57a07fd3bcd
Bilal Mir <bilalmir135@gmail.com>
2022-12-16 18:21:43 +0500

    Test/unipilot (#4)
    * chore(utils): extraction
    
    * test(unipilot): unipilot event unit testing
    
    * docs(README): description of adapters

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
 export * from "./projects";
-
-// import "./test";
-// import { extraction } from "./utils/extraction";
-// import { constants } from "@spockanalytics/base";
-
-// extraction(constants.Chain.ETHEREUM, "0x384a0a4f3718ba92ffa9d93f6659f50a0f10f7877e964b41465231466fbd396c");
