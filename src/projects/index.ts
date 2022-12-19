3f18db783bd3b365186ae2572ba7c6d9bbfaf045
BilalMir135 <bilalmir135@gmail.com>
2022-12-19 17:26:57 +0500

    chore(projects): remove unipilot

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,7 +1,7 @@
-import UnipilotAdapter from "./unipilot";
+// import UnipilotAdapter from "./unipilot";
 // import UniswapAdapter from "./uniswap";
 import { types } from "@spockanalytics/base";
 
 console.log("Adapters updated 4");
 
-export const Adapters: types.Adapter[] = [UnipilotAdapter];
+export const Adapters: types.Adapter[] = [];
