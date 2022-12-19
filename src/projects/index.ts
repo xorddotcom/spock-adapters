e4d47997ab5a15a08eb0bb09b3ea61ade15cb0ef
BilalMir135 <bilalmir135@gmail.com>
2022-12-19 13:51:16 +0500

    chore(log): add update log

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -2,6 +2,6 @@ import UnipilotAdapter from "./unipilot";
 // import UniswapAdapter from "./uniswap";
 import { types } from "@spockanalytics/base";
 
-console.log("Adapters updated");
+console.log("Adapters updated 2");
 
 export const Adapters: types.Adapter[] = [UnipilotAdapter];
