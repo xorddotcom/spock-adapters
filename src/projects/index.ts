a242355572cd294aef6533cf5a6c80d8075de5b6
BilalMir135 <bilalmir135@gmail.com>
2023-01-27 21:21:07 +0500

    feat(zharta): add usd conversion of token amount

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -2,6 +2,13 @@ import BullionFxAdapter from "./bullionfx";
 import DafiAdapter from "./dafi";
 import TetuEarnAdapter from "./tetu";
 import UnipilotAdapter from "./unipilot";
+import ZhartaAdapter from "./zharta";
 import { types } from "@spockanalytics/base";
 
-export const Adapters: types.Adapter[] = [BullionFxAdapter, DafiAdapter, TetuEarnAdapter, UnipilotAdapter];
+export const Adapters: types.Adapter[] = [
+  BullionFxAdapter,
+  DafiAdapter,
+  TetuEarnAdapter,
+  UnipilotAdapter,
+  ZhartaAdapter,
+];
