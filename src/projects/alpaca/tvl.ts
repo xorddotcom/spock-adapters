535927b678bf612a1360c2161d88b7de175ebfd3
Ijlal Ishaq <60582132+Ijlal-Ishaq@users.noreply.github.com>
2023-06-21 16:37:42 +0500

    Alpaca/feat (#26)
    * feat: alpaca adapter
    
    * refactor: alpaca events updated
    
    * refactor: alpaca block update
    
    * refactor: alpaca block updated
    
    ---------
    
    Co-authored-by: Ijlal-Ishaq <ijlalishaq2019gmail.com>

--- src/projects/alpaca/tvl.ts
+++ src/projects/alpaca/tvl.ts
@@ -1,6 +1,7 @@
 import { SummedBalances, sumMultipleBalance } from "../../utils/sumBalances";
 import { abi, constants } from "@spockanalytics/base";
 import axios from "axios";
+import { Interface } from "ethers/lib/utils";
 
 async function getMoneyMarket() {
   const moneyMarketRes = await axios.get(
@@ -12,7 +13,7 @@ async function getMoneyMarket() {
   };
 }
 
-export async function computeTVL(chain: constants.Chain, block: number) {
+export async function computeLendingTVL(chain: constants.Chain, block: number) {
   const balances: SummedBalances = {};
 
   const { moneyMarketDiamond, markets }: { moneyMarketDiamond: string; markets: string[] } = await getMoneyMarket();
@@ -27,6 +28,29 @@ export async function computeTVL(chain: constants.Chain, block: number) {
   const marketBalances = await abi.erc20MultiBalanceOf(marketBalancesInput, chain, block);
 
   sumMultipleBalance(balances, marketBalances);
+
+  return balances;
+}
+
+export async function computeBorrowTVL(chain: constants.Chain, block: number) {
+  const balances: SummedBalances = {};
+
+  const { moneyMarketDiamond, markets }: { moneyMarketDiamond: string; markets: string[] } = await getMoneyMarket();
+
+  const debtValues = await abi.Multicall.singleContractMultipleData({
+    address: moneyMarketDiamond,
+    blockNumber: block,
+    chain,
+    fragment: "getGlobalDebtValueWithPendingInterest",
+    contractInterface: new Interface([
+      "function getGlobalDebtValueWithPendingInterest(address _token) external view returns (uint256)",
+    ]),
+    callInput: markets,
+  });
+
+  markets.forEach((e, i) => {
+    balances[e.toLowerCase()] = debtValues[i].output.toString();
+  });
 
   return balances;
 }
