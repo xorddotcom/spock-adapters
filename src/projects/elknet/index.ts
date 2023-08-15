919f075dc12790bf07e01e382184e9f567ac5418
BilalMir135 <bilalmir135@gmail.com>
2023-08-15 22:06:55 +0500

    feat(elknet): add elknet adapter

---
+++ src/projects/elknet/index.ts
@@ -0,0 +1,130 @@
+import { stakingTvl } from "../../utils/staking";
+import { sumBalancesUSD, tokenBalanceUSD } from "../../utils/sumBalances";
+import { computeTVL } from "./tvl";
+import { BurnEventObject, MintEventObject } from "./types/ElkPair";
+import { StakedEventObject, WithdrawnEventObject } from "./types/Staking";
+import { BURN, MINT, elkPair, pairAddresses, elk_Pair, Label, ELK_TOKEN, staking, STAKE, UNSTAKE } from "./utils";
+import { constants, types, utils } from "@spockanalytics/base";
+
+export async function mintEvent(event: types.Event<MintEventObject>) {
+  const pair = await elk_Pair.getPool(event.address, event.chain);
+  if (pair) {
+    const [block, transaction] = await Promise.all([event.block, event.transaction]);
+    const totalSum = await sumBalancesUSD(
+      [
+        { token: pair.token0, balance: event.params.amount0 },
+        { token: pair.token1, balance: event.params.amount1 },
+      ],
+      event.chain,
+      block.timestamp,
+    );
+    return utils.ProtocolValue.contribution({
+      label: Label.ADD_LIQUIDITY,
+      value: parseFloat(totalSum.toString()),
+      user: transaction.from,
+    });
+  }
+}
+
+export async function burnEvent(event: types.Event<BurnEventObject>) {
+  const pair = await elk_Pair.getPool(event.address, event.chain);
+  if (pair) {
+    const [block, transaction] = await Promise.all([event.block, event.transaction]);
+    const totalSum = await sumBalancesUSD(
+      [
+        { token: pair.token0, balance: event.params.amount0 },
+        { token: pair.token1, balance: event.params.amount1 },
+      ],
+      event.chain,
+      block.timestamp,
+    );
+    return utils.ProtocolValue.extraction({
+      label: Label.REMOVE_LIQUIDITY,
+      value: parseFloat(totalSum.toString()),
+      user: transaction.from,
+    });
+  }
+}
+
+export async function stakeEvent(event: types.Event<StakedEventObject>) {
+  const block = await Promise.resolve(event.block);
+  const elkToken = ELK_TOKEN[event.chain];
+
+  if (elkToken && event.params.amount) {
+    const elkAmount = await tokenBalanceUSD(
+      { token: elkToken, balance: event.params.amount },
+      event.chain,
+      block.timestamp,
+    );
+
+    return utils.ProtocolValue.contribution({
+      label: Label.STAKE,
+      value: parseFloat(elkAmount.toString()),
+      user: event.params.user,
+    });
+  }
+}
+
+export async function withdrawnEvent(event: types.Event<WithdrawnEventObject>) {
+  const block = await Promise.resolve(event.block);
+  const elkToken = ELK_TOKEN[event.chain];
+
+  if (elkToken && event.params.amount) {
+    const elkAmount = await tokenBalanceUSD(
+      { token: elkToken, balance: event.params.amount },
+      event.chain,
+      block.timestamp,
+    );
+
+    return utils.ProtocolValue.extraction({
+      label: Label.UNSTAKE,
+      value: parseFloat(elkAmount.toString()),
+      user: event.params.user,
+    });
+  }
+}
+
+const elknetAdapter: types.Adapter = {
+  appKey: "",
+  transformers: {
+    [constants.Chain.POLYGON]: [
+      {
+        contract: elkPair,
+        getAddresses: pairAddresses,
+        eventHandlers: {
+          [MINT]: mintEvent,
+          [BURN]: burnEvent,
+        },
+        startBlock: 22757600,
+      },
+      {
+        contract: staking,
+        address: "0x57a1ce7686f3b2ab61f5191c76361f985b57e0fa",
+        eventHandlers: {
+          [STAKE]: stakeEvent,
+          [UNSTAKE]: withdrawnEvent,
+        },
+        startBlock: 26971152,
+      },
+    ],
+  },
+  tvlExtractors: {
+    [constants.Chain.POLYGON]: [
+      {
+        category: types.TVL_Category.TVL,
+        extractor: computeTVL,
+        startBlock: 22757600,
+      },
+      {
+        category: types.TVL_Category.STAKING,
+        extractor: stakingTvl(
+          "0x57a1ce7686f3b2ab61f5191c76361f985b57e0fa",
+          ELK_TOKEN[constants.Chain.POLYGON]?.address ?? "",
+        ),
+        startBlock: 26971152,
+      },
+    ],
+  },
+};
+
+export default elknetAdapter;
---
+++ src/projects/elknet/utils.ts
@@ -0,0 +1,48 @@
+import { AddressMap, PartialChainRecord } from "../../types/chain";
+import { PartialTokenDecimals } from "../../types/token";
+import { factoryAddressMapping, AddressMappingResult } from "../../utils/addressMapping";
+import { Pool, uniswapV2_Pair } from "../../utils/pool";
+import { ElkFactory__factory, ElkPair__factory, ElkFactory, ElkPair, Staking__factory, Staking } from "./types";
+import { constants } from "@spockanalytics/base";
+
+// contract interfaces
+export const elkFactory = ElkFactory__factory.createInterface();
+export const elkPair = ElkPair__factory.createInterface();
+export const staking = Staking__factory.createInterface();
+
+// contract events
+export const MINT = elkPair.getEventTopic(elkPair.getEvent("Mint"));
+export const BURN = elkPair.getEventTopic(elkPair.getEvent("Burn"));
+export const STAKE = staking.getEventTopic(staking.getEvent("Staked"));
+export const UNSTAKE = staking.getEventTopic(staking.getEvent("Withdrawn"));
+
+//types
+export enum Label {
+  ADD_LIQUIDITY = "Add Liquidity",
+  REMOVE_LIQUIDITY = "Remove Liquidity",
+  STAKE = "Stake",
+  UNSTAKE = "Unstake",
+}
+
+// constants
+const ELK_FACTORY: AddressMap = {
+  [constants.Chain.POLYGON]: "0xe3bd06c7ac7e1ceb17bdd2e5ba83e40d1515af2a",
+};
+
+export const ELK_TOKEN: PartialChainRecord<PartialTokenDecimals> = {
+  [constants.Chain.POLYGON]: { address: "0xeeeeeb57642040be42185f49c52f7e9b38f8eeee", decimals: 18 },
+};
+
+// helper functions
+export const elk_Pair = new Pool(uniswapV2_Pair<ElkPair>(elkPair));
+
+export async function pairAddresses(chain: constants.Chain, blockNumber?: number): AddressMappingResult {
+  return await factoryAddressMapping<ElkFactory>({
+    address: ELK_FACTORY[chain] ?? "",
+    chain,
+    contractInterface: elkFactory,
+    lengthFragment: "allPairsLength",
+    addressFragment: "allPairs",
+    blockNumber,
+  });
+}
